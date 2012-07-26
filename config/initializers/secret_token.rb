13ba291fe898025411a9eab7eeab2f229adedfc0
tim ng <ting@zappos.com>
2012-07-25 20:02:10 -0700

    generate genesis application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genesis::Application.config.secret_token = '1801c428d4cf60560716e73bf64b67686cce7f393d2ac9d6906ca562caa2fdca570c5fcaaf314a03152b388c49c40ebc1930b1d598b67600982e14a4399066bd'
